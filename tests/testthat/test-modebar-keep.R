b439deaab3ca26b4f6a37c6d891514140446a2e6
Stefan Moog <moogs@gmx.de>
2020-06-18 14:18:07 +0200

    Change naming of vars. "mb_" instead of "modebar_".

--- tests/testthat/test-modebar-keep.R
+++ tests/testthat/test-modebar-keep.R
@@ -2,55 +2,55 @@ context("Configuration Options")
 
 p <- plot_ly(iris, x = ~Sepal.Width, y = ~Sepal.Length, mode = 'markers', type = 'scatter')
 
-modebar_names <- modebar_names()
-len_modebar_names <- length(modebar_names)
+mb_names <- modebar_names()
+len_mb_names <- length(mb_names)
 
 modebar_diff <- function(x, y = NULL) {
-  y <- y %||% modebar_names
+  y <- y %||% mb_names
   as.list(setdiff(unlist(y), unlist(x)))
 }
 test_that("modeBarButtonsToKeep works as expected", {
-  modebar_keep <- list("toImage", "zoom2d")
-  modebar_remove <- modebar_diff(modebar_keep)
+  mb_keep <- list("toImage", "zoom2d")
+  mb_remove <- modebar_diff(mb_keep)
   
-  L <- p  %>% config(modeBarButtonsToKeep = modebar_keep)
+  L <-  config(p, modeBarButtonsToKeep = mb_keep)
   
-  L_modebar_remove <- L$x$config$modeBarButtonsToRemove
+  L_mb_remove <- L$x$config$modeBarButtonsToRemove
   
-  expect_equal(L_modebar_remove, modebar_remove)
-  expect_equal(length(L_modebar_remove), len_modebar_names - length(modebar_keep))
+  expect_equal(L_mb_remove, mb_remove)
+  expect_equal(length(L_mb_remove), len_mb_names - length(mb_keep))
 })
 
 test_that("modeBarButtonsToKeep works when empty", {
-  modebar_keep <- list()
-  modebar_remove <- modebar_diff(modebar_keep)
+  mb_keep <- list()
+  mb_remove <- modebar_diff(mb_keep)
   
-  L <- p  %>% config(modeBarButtonsToKeep = modebar_keep)
-  L_modebar_remove <- L$x$config$modeBarButtonsToRemove
+  L <- config(p, modeBarButtonsToKeep = mb_keep)
+  L_mb_remove <- L$x$config$modeBarButtonsToRemove
   
-  expect_equal(L_modebar_remove, as.list(modebar_names))
-  expect_equal(length(L_modebar_remove), len_modebar_names)
+  expect_equal(L_mb_remove, as.list(mb_names))
+  expect_equal(length(L_mb_remove), len_mb_names)
 })
 
 test_that("modeBarButtonsToKeep works when all names given", {
-  modebar_keep <- as.list(modebar_names)
-  modebar_remove <- modebar_diff(modebar_keep)
+  mb_keep <- as.list(mb_names)
+  mb_remove <- modebar_diff(mb_keep)
   
-  L <- p  %>% config(modeBarButtonsToKeep = modebar_keep)
+  L <-  config(p, modeBarButtonsToKeep = mb_keep)
   
-  L_modebar_remove <- L$x$config$modeBarButtonsToRemove
+  L_mb_remove <- L$x$config$modeBarButtonsToRemove
   
-  expect_equal(L_modebar_remove, list())
-  expect_equal(length(L_modebar_remove), 0)
+  expect_equal(L_mb_remove, list())
+  expect_equal(length(L_mb_remove), 0)
 })
 
 test_that("modeBarButtonsToKeep works with vectors of length 1", {
-  modebar_keep <- c("toImage")
-  modebar_remove <- modebar_diff(modebar_keep)
+  mb_keep <- c("toImage")
+  mb_remove <- modebar_diff(mb_keep)
   
-  L <- p  %>% config(modeBarButtonsToKeep = modebar_keep)
-  L_modebar_remove <- L$x$config$modeBarButtonsToRemove
+  L <-  config(p, modeBarButtonsToKeep = mb_keep)
+  L_mb_remove <- L$x$config$modeBarButtonsToRemove
   
-  expect_equal(L_modebar_remove, modebar_remove)
-  expect_equal(length(L_modebar_remove), len_modebar_names - 1)
+  expect_equal(L_mb_remove, mb_remove)
+  expect_equal(length(L_mb_remove), len_mb_names - 1)
 })
